b5edab85ff0e8f28ccca58ed5eeb9467edcae31a
Ellen1997 <ellen-holmgren@yh.nackademin.se>
2025-04-16 17:14:21 +0200

    ändringar vilken modal som visas samt vad användarnamn

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,15 @@ const password = document.querySelector("#password-input-register");
 const logoutBtn = document.querySelector("#logoutBtn");
 
 openModalBtn.addEventListener("click", () => {
-  showModal(loginModal);
+  const token = localStorage.getItem("token");
+  if (token) {
+    const name = localStorage.getItem("name");
+    const accountHeader = document.querySelector("#accountModal h2");
+    accountHeader.textContent = `HÄRLIGT ATT DU ÄR TILLBAKA${name.toUpperCase()}!`;
+    showModal(accountModal);
+  } else {
+    showModal(loginModal);
+  }
 });
 
 closeModal.addEventListener("click", () => {
@@ -70,6 +78,7 @@ loginForm.addEventListener("submit", async (e) => {
 
     if (response.ok) {
       localStorage.setItem("token", data.token);
+      localStorage.setItem("name", data.data.user.email.split("@")[0]);
       document.getElementById("loginText").innerHTML = "Ditt Konto";
       hideModal(loginModal);
       // showModal(accountModal);
@@ -123,7 +132,9 @@ registerForm.addEventListener("submit", async (e) => {
 
     if (response.ok) {
       localStorage.setItem("token", data.token);
+      localStorage.setItem("name", data.data.user.email.split("@")[0]);
       hideModal(createAccountModal);
+      
 
       // Om användaren försöker gå till kassan men inte registrerad,skicka vidare till checkout efter registrerad:
       if (localStorage.getItem("goToCheckoutAfterLogin")) {
@@ -148,6 +159,7 @@ registerForm.addEventListener("submit", async (e) => {
 
 logoutBtn.addEventListener("click", () => {
   localStorage.removeItem("token");
+  localStorage.removeItem("name");
   document.getElementById("loginText").innerHTML = "Logga in";
   hideModal(accountModal);
   alert("Du har loggats ut.");
