f8c819101cf9008aa95ba8dc8c90649dd9f9c867
yolandaatt <yolandaatteyeh3@gmail.com>
2025-04-23 23:31:50 +0200

    style

--- scripts/my-pages.js
+++ scripts/my-pages.js
@@ -108,15 +108,16 @@ const fetchCustomerData = async () => {
 };
 
 editBtn.addEventListener('click', () => {
-    editName.placeholder = customerNameElement.textContent.trim() || "Namn...";
-    editEmail.placeholder = customerEmailElement.textContent.trim() || "Email...";
-    editPhone.placeholder = customerPhoneElement.textContent.trim() || "Telefon...";
+
+    editName.placeholder = customerNameElement.textContent.trim() ? "" : "Namn...";
+    editEmail.placeholder = customerEmailElement.textContent.trim() ? "" : "Email...";
+    editPhone.placeholder = customerPhoneElement.textContent.trim() ? "" : "Telefon...";
 
     
     editName.value = customerNameElement.textContent.trim() || "";
     editEmail.value = customerEmailElement.textContent.trim() || "";
     editPhone.value = customerPhoneElement.textContent.trim() || "";
-    
+
     editProfileModal.style.display = "block";
 });
 
